3a091fae766856977b12f5ed1fcbdca22e22be55
kostia6411 <91974940+kostia6411@users.noreply.github.com>
2025-06-05 18:19:07 +0300

    sorting imports

--- render_website.py
+++ render_website.py
@@ -1,9 +1,9 @@
 import json
+import os
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
-import os
-
 
 os.makedirs("pages", exist_ok=True)
 
